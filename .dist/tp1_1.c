73cc061ef13b61f2eb50198685aa4d8b2a05418d
Pablo Nacusse <grandnacusse@gmail.com>
2021-04-04 13:11:44 -0300

    primer punto terminado

--- .dist/tp1_1.c
+++ .dist/tp1_1.c
@@ -2,7 +2,17 @@
 
 int main()
 {
-    
+    int variable = 29;
+    int *pVariable;
+    pVariable = &variable;
+
+    printf("Contenido del puntero: %d\n", *pVariable);
+    printf("Direccion de memoria almacenada por el puntero: %u\n", pVariable);
+    printf("Direccion de memoria de la variable: %u\n", &variable);
+    printf("Direccion de memoria del puntero: %u\n", &pVariable);
+    printf("Tamanio de memoria utilizado por la variable: %d", sizeof(variable));
+
+    getchar();
 
     return 0;
 }
